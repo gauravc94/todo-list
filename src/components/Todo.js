f7de93f088b8c7dbacc5f7c80e816d510ebe3757
gauravc94 <gcserves86@gmail.com>
2024-02-22 03:37:26 +0530

    Feat: Added the edit and delete features to every single task in the todolist.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,9 @@
-import React from 'react' 
+import React, { useState } from 'react' 
 
 export default function Todo({id, taskName, taskDescription, todoStatus, todos, setTodos}) {
+    const [isEditing, setIsEditing] = useState(false)
+    const [editedTaskName, setEditedTaskName] = useState(taskName)
+    const [editedTaskDescription, setEditedTaskDescription] = useState(taskDescription)
 
     const handleTodoStatusChange = (e) => {
         const newTodos = todos.map(todo => todo.id === id ? { ...todo, status: e.target.value} : todo)
@@ -8,13 +11,56 @@ export default function Todo({id, taskName, taskDescription, todoStatus, todos, 
     }
 
     const handleEditClick = () => {
-        console.log("edit")
+        setIsEditing(true)
+    }
+
+    const handleSaveClick = () => {
+        const newTodos = todos.map(todo => todo.id === id ? 
+            {...todo, name: editedTaskName, description: editedTaskDescription} : todo)
+
+        setTodos(newTodos)
+        setIsEditing(false)
     }
 
     const handleDeleteClick = () => {
-        console.log("delete")
+        const newTodos = todos.filter(todo => todo.id !== id)
+        setTodos(newTodos)
     }
 
+    return (
+        <div className='todo'>
+            {isEditing ? (
+                <>
+                    <input type="text" value={editedTaskName} 
+                    onChange={(e) => setEditedTaskName(e.target.value)} />
+                    <input type="text" value={editedTaskDescription} 
+                    onChange={(e) => setEditedTaskDescription(e.target.value)}/>
+                </>
+            ) : (
+                <>
+                    <p>Name : {taskName}</p>
+                    <p>Description : {taskDescription}</p>
+                </>
+            )}
+            <label htmlFor="status">Status</label>
+            <select name="status" value={todoStatus} onChange={handleTodoStatusChange}>
+                <option value="completed">Completed</option>
+                <option value="notCompleted">Not Completed</option>
+            </select>
+            {isEditing ? (
+                <button type="button" onClick={handleSaveClick}>Save</button>
+            ) : (
+                <>
+                    <button type="button" onClick={handleEditClick}>Edit</button>
+                    <button type="button" onClick={handleDeleteClick}>Delete</button>
+                </>
+            )}
+        </div>
+    )
+}
+
+
+/*
     return (
         <div>
             Todo
@@ -29,4 +75,4 @@ export default function Todo({id, taskName, taskDescription, todoStatus, todos, 
             <button type="button" onClick={handleDeleteClick}>Delete</button>
         </div>
     )
-}
+*/
